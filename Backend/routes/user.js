ffb0ad1ddf3cc3c30affb6b3a2375f2e92ed7448
bbartika <137605070+bbartika@users.noreply.github.com>
2024-09-02 02:34:18 +0530

    direct route for login,signup feature

---
+++ Backend/routes/user.js
@@ -0,0 +1,8 @@
+const express=require('express')
+const router=express.Router()
+const userController=require('../controller/user')
+
+router.post('/signup',userController.signUp)
+router.post('/login',userController.logIn)
+
+module.exports=router         
