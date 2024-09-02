3d9d8217ae87e9dc913996b206912dbf0a240d2e
bbartika <137605070+bbartika@users.noreply.github.com>
2024-09-02 09:54:00 +0530

    setup navigation

--- public/src/navigation.js
+++ public/src/navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './components/User/signup';
 import Login from './components/User/login';
-import Dashboard from './components/Dashboard';
+
 
 function navigation() {
     return (
@@ -11,7 +11,6 @@ function navigation() {
             <Routes>
                 <Route path="/signup" element={<User/signup />} />
                 <Route path="/login" element={<User/login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
     );
